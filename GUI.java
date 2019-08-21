01519ba9dd46cba1f6de2f1c4879e8f774708165
Ajaeb Ismael <ajaebabdurahman@outlook.com>
2019-08-21 09:44:43 -0400

    created GUI

--- GUI.java
+++ GUI.java
@@ -1,8 +1,9 @@
 package finalProject;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class GUI extends JFrame{
+public class GUI extends JFrame implements ActionListener{
 	public GUI() {
 		String[] buttons = {"1","2","3","+","*","4","5","6","-","**","7","8","9","/","backspace",".","0","=","%","CE"};
 	    JPanel calculatorPanel = new JPanel();
@@ -10,20 +11,16 @@ public class GUI extends JFrame{
 	    for(int i =0; i< buttons.length;i++) {
 	    	calculatorPanel.add(new JButton(buttons[i]));
 	    }
-	    JPanel outputPanel = new JPanel();
-	    outputPanel.setLayout(new GridLayout(2,1));
-	    outputPanel.add(new JTextField());
-	    outputPanel.add(new JTextField());
-	    
 	    JPanel mainContainer = new JPanel(new BorderLayout());
-	    mainContainer.add(outputPanel, BorderLayout.NORTH);
-	    mainContainer.add(calculatorPanel);
+	    mainContainer.add(new JTextField());
+	    mainContainer.add(calculatorPanel, BorderLayout.SOUTH);
+	    add(mainContainer);
 	    
 	}
 	public static void main(String[] args) {
 		GUI frame = new GUI(); 
 		frame.setTitle("Simple Calculator");
-		frame.setSize(400, 400); 
+		frame.setSize(300, 400); 
 		frame.setLocationRelativeTo(null); 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true); 
