d35fb31a6e0bac8d612b2bcc7cc700ad0e5af4d0
AjIsmael <49954017+AjIsmael@users.noreply.github.com>
2019-08-21 23:04:06 -0400

    some extent of calculator function implemented
    need to fix when a negative number is inserted.
    if there is time also make number to be read from the keyboard click.

--- GUI.java
+++ GUI.java
@@ -2,6 +2,8 @@ package finalProject;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.regex.Pattern;
+import com.sun.xml.internal.ws.util.StringUtils;
 
 public class GUI extends JFrame implements ActionListener{
 	public static String[] buttons = {"1","2","3","+","*","4","5","6","-","**","7","8","9","/","backspace",".","0","=","%","CE"};
@@ -11,7 +13,9 @@ public class GUI extends JFrame implements ActionListener{
 	    JPanel calculatorPanel = new JPanel();
 	    calculatorPanel.setLayout(new GridLayout(4,5));
 	    for(int i =0; i< buttons.length;i++) {
-	    	calculatorPanel.add(new JButton(buttons[i]));
+	    	JButton button = new JButton(buttons[i] );
+	    	button.addActionListener(this);
+	    	calculatorPanel.add(button);
 	    }
 	    JPanel mainContainer = new JPanel(new BorderLayout());
 	    mainContainer.add(textField);
@@ -29,9 +33,67 @@ public class GUI extends JFrame implements ActionListener{
 	}
 	public void actionPerformed(ActionEvent e) {
 			 String action = e.getActionCommand();
-			 for(int i = 0, i<buttons.length; i++){
+			 for(int i = 0; i<buttons.length; i++){
 				 if (action.equals(buttons[i])){
-
+					 String firstState = textField.getText();
+					 if(buttons[i].equals("backspace")) {
+						 firstState = firstState.substring(0, firstState.length() - 1);
+						 textField.setText(firstState); 
+					 }
+					 else if(buttons[i].equals("CE")){
+						 firstState = "";
+						 textField.setText(firstState);
+					 }
+					 else if(buttons[i].equals("+")||buttons[i].equals("-")||buttons[i].equals("/")||buttons[i].equals("%")||buttons[i].equals("*")||buttons[i].equals("**")) {
+						 if(firstState.contains("+")||firstState.contains("-")||firstState.contains("*")||firstState.contains("/")||firstState.contains("**")||firstState.contains("%")) {
+							 String sign = findArthemiticSign(firstState);
+							 //need to consider negative number
+							 String[] arguments = firstState.split(Pattern.quote(sign));
+							 double argument1 = Double.parseDouble(arguments[0]);
+							 double argument2 = Double.parseDouble(arguments[1]);
+							 double answer=0;
+							 if(sign.equals("+")) {
+								 answer = addition(argument1, argument2);
+							 } else if(sign.equals("-")) {
+								 answer = subtraction(argument1,argument2);
+							 }else if(sign.equals("*")) {
+								 answer = multiplication(argument1,argument2);
+							 }else if(sign.equals("/")) {
+								 answer = division(argument1,argument2);
+							 }else if(sign.equals("%")) {
+								 answer = modulus(argument1,argument2);
+							 }else if(sign.equals("-")) {
+								 answer = subtraction(argument1,argument2);
+							 } else {
+								 answer = power(argument1,argument2);
+							 }
+							 textField.setText(Double.toString(answer));
+						 }
+						 else {
+							textField.setText(firstState+buttons[i]);
+						 }
+					 }
+					 else {
+						 textField.setText(firstState+buttons[i]);
+					 }
 				 }
 			 }
+	}
+	public static String findArthemiticSign(String str) {
+		String arthemiticSign;
+		if (str.contains("+")) {
+			arthemiticSign="+";
+		} else if(str.contains("-")) {
+			arthemiticSign="-";
+		}else if(str.contains("/")) {
+			arthemiticSign="/";
+		}else if(str.contains("*")) {
+			arthemiticSign="*";
+		}else if(str.contains("%")) {
+			arthemiticSign="%";
+		}else {
+			arthemiticSign="**";
+		}
+		return arthemiticSign;
+	}
 }
