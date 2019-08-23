f1fab9b24e3bd717a29b5ecf6358ac89c72e71d3
AjIsmael <49954017+AjIsmael@users.noreply.github.com>
2019-08-22 23:31:37 -0400

    Fixed some bugs
    Needs work on validation.

--- GUI.java
+++ GUI.java
@@ -7,6 +7,7 @@ import finalProject.Calculator;
 public class GUI extends JFrame implements ActionListener{
 	public static String[] buttons = {"1","2","3","+","*","4","5","6","-","**","7","8","9","/","backspace",".","0","=","%","CE"};
 	public JTextField textField = new JTextField();
+	public static int flag = 0;
 	public GUI() {
 
 	    JPanel calculatorPanel = new JPanel();
@@ -35,25 +36,53 @@ public class GUI extends JFrame implements ActionListener{
 			 for(int i = 0; i<buttons.length; i++){
 				 if (action.equals(buttons[i])){
 					 String firstState = textField.getText();
+				
+					 if(firstState.equals("Infinity")||firstState.equals("-Infinity")) {
+						 firstState = ""; 
+						 }
+				 
 					 if(buttons[i].equals("backspace")) {
 						 firstState = firstState.substring(0, firstState.length() - 1);
 						 textField.setText(firstState); 
 					 }
-					 else if(buttons[i].equals("="))
 					 else if(buttons[i].equals("CE")){
 						 firstState = "";
 						 textField.setText(firstState);
 					 }
-					 else if(buttons[i].equals("+")||buttons[i].equals("-")||buttons[i].equals("/")||buttons[i].equals("%")||buttons[i].equals("*")||buttons[i].equals("**")) {
+					 else if(buttons[i].equals("+")||buttons[i].equals("-")||buttons[i].equals("/")||buttons[i].equals("%")||buttons[i].equals("*")||buttons[i].equals("**")||buttons[i].equals("=")) {
+						 
 						 if(firstState.contains("+")||firstState.contains("-")||firstState.contains("*")||firstState.contains("/")||firstState.contains("**")||firstState.contains("%")) {
 							 String sign = findArthemiticSign(firstState);
 							 //need to consider negative number
-							 if(firstState.indexOf(sign)==(firstState.length()-1)) {
+							 if(firstState.indexOf(sign)==(firstState.length()-1)||firstState.charAt(firstState.length()-1)=='-') {
+								 if(buttons[i].equals("-")) {
+									 textField.setText(firstState + buttons[i]);
+									 break;
+								 }
 								 firstState = firstState.substring(0, firstState.length() - 1);
 								 textField.setText(firstState + buttons[i]);
 								 break;
 							 }
-							 String[] arguments = firstState.split(Pattern.quote(sign));
+							 if(sign.equals("**")) {
+								 if(firstState.indexOf(sign)==(firstState.length()-2)) {
+									 firstState = firstState.substring(0, firstState.length() - 2);
+									 textField.setText(firstState + buttons[i]);
+									 break;
+								 }
+								 
+							 }
+							 String[] arguments= {"",""};
+							 if(sign.equals("-")) {
+								 if(flag == 0) {
+									 int index = firstState.indexOf("-", 1);
+									 arguments[0] = firstState.substring(0, index);
+									 arguments[1] = firstState.substring(index+1);
+								 } else {
+									 textField.setText(firstState + buttons[i]);
+								 }
+							 }else {
+							 arguments = firstState.split(Pattern.quote(sign));
+							 }
 							 double argument1 = Double.parseDouble(arguments[0]);
 							 double argument2 = Double.parseDouble(arguments[1]);
 							 double answer=0;
@@ -72,20 +101,41 @@ public class GUI extends JFrame implements ActionListener{
 							 } else {
 								 answer = Calculator.multiplication(argument1,argument2);
 							 }
-							 textField.setText(String.format("%.2f", answer)+buttons[i]);
+							 String answerInString = String.format("%.2f", answer);
+							 if (answerInString.equals("Infinity")||answerInString.equals("-Infinity")) {
+								 textField.setText(answerInString);
+							 }else {
+								 if(buttons[i].equals("=")) {
+									 textField.setText(answerInString);
+									 flag = 1;
+								 } else {
+									 textField.setText(answerInString + buttons[i]);
+									 flag=0;
+								 }
+							 }
 						 }
 						 else {
 							 if(firstState.length()==0) {
 								 textField.setText("0" +firstState+buttons[i]);
+							 } else if (buttons[i].equals("=")) {
+								 textField.setText(firstState);
+								 flag=1;
 							 }
 							 else{
 								 textField.setText(firstState+buttons[i]);
+								 flag=0;
+								
 							 }
 						 }
 					 } 
 					 
 					 else {
-						 textField.setText(firstState+buttons[i]);
+						 if(flag==1) {
+							textField.setText(buttons[i]);
+							flag=0;
+						 } else {
+							 textField.setText(firstState+buttons[i]);
+						 }
 						 
 					 }
 				 }
@@ -95,16 +145,16 @@ public class GUI extends JFrame implements ActionListener{
 		String arthemiticSign;
 		if (str.contains("+")) {
 			arthemiticSign="+";
-		} else if(str.contains("-")) {
-			arthemiticSign="-";
-		}else if(str.contains("/")) {
+		} else if(str.contains("/")) {
 			arthemiticSign="/";
 		}else if(str.contains("**")) {
 			arthemiticSign="**";
 		}else if(str.contains("%")) {
 			arthemiticSign="%";
-		}else {
+		}else if(str.contains("*")) {
 			arthemiticSign="*";
+		}else {
+			arthemiticSign="-";
 		}
 		return arthemiticSign;
 	}
